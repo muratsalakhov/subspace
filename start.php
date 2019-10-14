58e4e02d14261bdd160e28116175d7ca9e03f8e2
muratsalakhov <muratsalakhov@gmail.com>
2019-10-14 20:19:03 +0400

    commit

---
+++ start.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Space-Sub</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700&display=swap&subset=cyrillic,cyrillic-ext,latin-ext" rel="stylesheet">
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="css/base.css">
+	<link rel="stylesheet" href="css/start.css">
+<body>
+
+
+<header>
+	<div class="container">
+		<div class="header-logo">
+			<a href="/">Space-Sub</a>
+		</div>
+		<div class="header-buttons">
+			<a href="authentification.php">Вход
+			<a href="registration.php">Регистрация</a>
+		</div>
+	</div>
+</header>
+
+
+<footer>
+	<div class="container">
+		<div class="footer-info">
+			<p>Создано в учебных целях.</p>
+		</div>
+	</div>
+</footer>
+
+<div id="unregistered" class="modalbackground">
+	<div class="modalwindow">
+		<h3>Ошибка!</h3>
+		<p>Авторизуйтесь для работы с сайтом</p>
+		<a href="start.php">Закрыть</a>
+	</div>
+</div>
+
+</body>
+</html>
