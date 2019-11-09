16c94b6cbb79d61de525ef1f25ffc81e18269f34
muratsalakhov <muratsalakhov@gmail.com>
2019-11-09 22:56:06 +0400

    Todo list added

--- main.php
+++ main.php
@@ -15,6 +15,9 @@
 	</header>
 	<section>
 		<div id="calendar-widget">
+			<div class="calendar-header">
+				<h1>Календарь</h1>
+			</div>
 			<table id="calendar">
 				<thead>
 					<tr><td><i class="fas fa-arrow-left"></i><td colspan="5"><td><i class="fas fa-arrow-right"></i>
@@ -24,7 +27,25 @@
 		</div>
 		<div class="tracker-widget">tracker-widget</div>
 		<div id="clock-widget"></div>
-		<div class="to-do-list-widget">to-do-list-widget</div>
+		<div id="todo-list-widget">
+			<div class="todo-list-header">
+                <h1>Заметки</h1>
+            </div>
+			<ul id="todo-list">
+                <li class="todo-item">
+                    <input class="checkbox" type="checkbox">
+                    <label class="title">Первая задача</label>
+                    <input class="textfield" type="text" autocomplete="off">
+                    <button class="edit"><i class="fas fa-edit"></i></button>
+                    <button class="delete"><i class="fas fa-trash-alt"></i></button>
+                </li>
+            </ul>
+
+            <form id="todo-form">
+                <input id="add-input" type="text" autocomplete="off">
+                <button id="add-button" type="submit">Добавить</button>
+            </form>
+		</div>
 		<div class="notes-widget">notes-widget</div>
 		<div class="timetable-widget">timetable-widget</div>
 		<div class="timer-widget">timer-widget</div>
@@ -39,5 +60,6 @@
 	</footer>
 <script src="js/clock-widget.js"></script>
 <script src="js/calendar-widget.js"></script>
+<script src="js/to-do-list-widget.js"></script>
 </body>
 </html>
