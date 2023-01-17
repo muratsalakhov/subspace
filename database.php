7050d7f7db0f16af3d420ece95aa83ceb02113b7
muratsalakhov <muratsalakhov@gmail.com>
2023-01-18 01:07:55 +0300

    feat: Получение данных подключения к БД из .env

--- database.php
+++ database.php
@@ -1,5 +1,17 @@
 <?php
-$connection = mysqli_connect('subspace', 'root', '');
-$select_db = mysqli_select_db($connection, 'subspace');
+
+require 'vendor/autoload.php';
+
+use Symfony\Component\Dotenv\Dotenv;
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__ . '/.env');
+
+$connection = mysqli_connect(
+    $_ENV["DB_HOSTNAME"],
+    $_ENV["DB_USERNAME"],
+    $_ENV["DB_PASSWORD"]
+);
+$select_db  = mysqli_select_db($connection, $_ENV["DB_HOSTNAME"]);
 session_start();
 ?>
