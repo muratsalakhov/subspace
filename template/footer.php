395dd5e243edbd4eb5f9035beb755adaf5cbac1e
muratsalakhov <muratsalakhov@gmail.com>
2023-01-20 00:03:51 +0300

    feat: Подключение футера для дашборда

---
+++ template/footer.php
@@ -0,0 +1,10 @@
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="../js/clock-widget.js"></script>
+        <script src="../js/calendar-widget.js"></script>
+        <script src="../js/to-do-list-widget.js"></script>
+        <script src="../js/timer-widget.js"></script>
+        <script src="../js/timetable-widget.js"></script>
+        <script src="../js/notes-widget.js"></script>
+        <script src="../js/tracker-widget.js"></script>
+    </body>
+</html>
